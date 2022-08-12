05ac722673e681702fc90fe64241a1503c4a424d
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2022-08-11 23:10:02 -0400

    Commit before bed

--- class_IHOMFAC.py
+++ class_IHOMFAC.py
@@ -19,20 +19,28 @@ class IHOMFAC:
         self.alpha = np.array(alpha).reshape(1,-1) # row vec
         self.beta = np.array(beta).reshape(1,-1) # row vec
         
-        # self.y = np.array([y_init]*2)
-        # self.u = np.array([u_init]*len(alpha)).reshape(-1,1) # col vec
-        self.y = np.array(list(range(1,3)))
-        self.u = np.array(list(range(1, len(alpha)+1))).reshape(-1,1) # col vec
+        self.y = np.array([y_init]*2)
+        self.u = np.array([u_init]*len(alpha)).reshape(-1,1) # col vec
+        # self.y = np.array(list(range(1,3)))
+        # self.u = np.array(list(range(1, len(alpha)+1))).reshape(-1,1) # col vec
         
-        self.u[1] = 1 # for testing
+        # self.u[1] = 1 # for testing
         
         self.phi_init = phi_init
         self.phi_init_sign = np.sign(phi_init)
-        self.phi = np.array([phi_init]*len(beta)).reshape(-1,1) # col vec
+        # self.phi = np.array([phi_init]*len(beta)).reshape(-1,1) # col vec
+        lst_phi = [0]*len(beta)
+        lst_phi[0] = phi_init
+        self.phi = np.array(lst_phi).reshape(-1,1) # col vec
+        
+        # self.phi = np.array([0]*len(beta)).reshape(-1,1) # col vec
+        # self.phi[0,0] = phi_init
         
         
     def param_est(self, y_meas):
+        # TODO: Replace 'shift' function with something else. This changes the zeros to something really small
         self.y = shift(self.y, 1, cval=y_meas) # shifts y arr right and replaces first val with new y meas
+        self.y[np.abs(self.y) < self.eps] = 0
         
         # TESTINGVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 #         print(np.shape(self.beta))
@@ -42,12 +50,14 @@ class IHOMFAC:
         del_y_t = self.y[0] - self.y[1] # TODO: Reshape this to ()
         
         # New phi estimate
-        phi_est = (sum_beta + (self.eta*del_u_t_1)*(del_y_t - del_u_t_1*sum_beta)/(self.mu + del_u_t_1**2))
+        phi_est = sum_beta + (self.eta*del_u_t_1)*(del_y_t - del_u_t_1*sum_beta)/(self.mu + del_u_t_1**2)
         
         if (phi_est <= self.eps) or (abs(del_u_t_1) <= self.eps) or (np.sign(phi_est) != self.phi_init_sign):
             phi_est = self.phi_init
-            
+        
+        # TODO: Replace 'shift' function with something else. This changes the zeros to something really small
         self.phi = shift(self.phi, [1,0], cval=phi_est) # shifts phi arr right and replaces first val with new phi est
+        self.phi[np.abs(self.phi) < self.eps] = 0
         
         
     def control_input(self, y_setpoint):
@@ -56,7 +66,10 @@ class IHOMFAC:
         error = y_setpoint - self.y[0]
         
         u_calc = ((self.phi[0]**2)*self.u[1] + self.lam*sum_alpha + self.ro*self.phi[0]*error)/coeff_denum
+        
+        # TODO: Replace 'shift' function with something else. This changes the zeros to something really small
         self.u = shift(self.u, [1,0], cval=u_calc) # shifts u arr right and replaces first val with new control input u
+        self.u[np.abs(self.u) < self.eps] = 0
         
         
     def run_algo(self, y_meas, y_setpoint):
--- reasearchPaper_implementation.py
+++ reasearchPaper_implementation.py
@@ -8,18 +8,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import pi
 import class_IHOMFAC
-# from class_IHOMFAC import IHOMFAC
 # plt.rcParams['figure.figsize'] = [20, 10]
 
 
 class data_gen:
     def __init__(self):
         # end of lists below (y and u) represent newest elements while first element is oldest
-        # self.lst_y = [0]*3 # list of measured outputs
-        # self.lst_u = [0]*2 # list of measured control inputs
+        self.lst_y = [0]*3 # list of measured outputs
+        self.lst_u = [0]*2 # list of measured control inputs
         
-        self.lst_y = list(range(1,4)) # list of measured outputs
-        self.lst_u = list(range(1,3))# list of measured control inputs
+        # self.lst_y = list(range(1,4)) # list of measured outputs
+        # self.lst_u = list(range(1,3))# list of measured control inputs
         
         self.t = 0
         
@@ -56,26 +55,50 @@ def desired_output(t):
 t = [i for i in range(1001)]
 lst_y_setpoint = [desired_output(t_i) for t_i in t]
 
+lst_y = [0]*3 # list of measured outputs
+lst_u = [0]*2 # list of measured control inputs
+
 eta = 0.8
 lam = 0.1
 mu = 0.01
 ro = 0.8
-eps = 5e-16
+eps = 1e-6
 alpha = [0.5, 0.25, 1/8, 1/8]
 beta = [0.5, 0.25, 1/8, 1/16, 1/32, 1/32]
 
-obj_data = data_gen()
-obj_IHOMFAC = class_IHOMFAC.IHOMFAC(eta, lam, mu, ro, eps, alpha, beta, phi_init=0.1)
+obj_IHOMFAC = class_IHOMFAC.IHOMFAC(eta, lam, mu, ro, eps, alpha, beta, phi_init=0.9)
+
+y_meas = 0 # initial output measurment
 
 for k in t:
-    y_meas = obj_data.output_meas(obj_IHOMFAC.u[0].reshape(()))
+    
     obj_IHOMFAC.run_algo(y_meas, lst_y_setpoint[k])
+    
+    lst_u.append(obj_IHOMFAC.u[0].reshape(())) # TODO: check if this is giving what we want
+     
+      # lst index [-1] signifies time t, [-2] signifies time (t-1) and so on
+    if k <= 500:
+        y_meas = lst_y[-1]/(1 + lst_y[-1]**2) + lst_u[-1]**3
+       
+    else:
+        y_meas = ((lst_y[-1]*lst_y[-2]*lst_y[-3]*lst_u[-2]*
+                (lst_y[-3] - 1) + round(k/500)*lst_u[-1])/
+                (1 + lst_y[-2]**2 + lst_y[-3]**2))
+   
+    # append new output measurment to end of list
+    lst_y.append(y_meas)
+
 
 plt.plot(t, lst_y_setpoint)
-plt.plot(t, obj_data.lst_y[3:])
+plt.plot(t, lst_y[3:])
 plt.grid()
 plt.ylim((-2,2))
 plt.xlim((0,1000))
 plt.show()
 
-plt.plot(t, obj_data.lst_u[2:])
+plt.plot(t, lst_u[2:])
+plt.show()
+
+plt.plot(t[:150], lst_y_setpoint[:150])
+plt.plot(t[:150], lst_u[2:152])
+
